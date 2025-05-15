503dace4ca4a47dcfe094019e950fcdf96238c41
gabe <gabeklav@fastmail.com>
2025-05-15 17:27:35 -0400

    update post processing for gamejay post

--- post-processing.sh
+++ post-processing.sh
@@ -1,2 +1,3 @@
 # remove repeated footnotes sections https://github.com/markdown-it/markdown-it-footnote/issues/53
 sed -i '67,83d' _site/posts/distroless/index.html
+sed -i '138,146d' _site/posts/Gamejay/index.html
